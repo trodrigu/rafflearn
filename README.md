d1685109ff125054833754087f490de75fd34142
Tommy Rodriguez <trodriguez91@icloud.com>
2015-05-23 13:52:30 -0700

    Add some views

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rafflearn
+
+A game designed to choose a set of winners from a pool of players.
--- app/controllers/rafflrs_controller.rb
+++ app/controllers/rafflrs_controller.rb
@@ -2,11 +2,12 @@ class RafflrsController < ApplicationController
   def index
   end
   def create
-    @raffle = Raffle.new(params[:raffle])
-    
+    @raffle = Rafflr.new(params[:raffle])
+    @raffle.save
+
   end
   def new
     @page_title = "New Raffle"
-    @raffle = Raffle.new
+    @raffle = Rafflr.new
   end
 end
